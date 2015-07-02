7e86ffb3876ba88b6e208e13dbbc8bb0db24c843
nacnudus <nacnudus@gmail.com>
2015-07-03 10:24:03 +1200

    Revert "Delete atom feeds"
    This reverts commit 05c5e93d7f8d93455a358effdb69528176dfbb27.

--- publishconf.py
+++ publishconf.py
@@ -13,6 +13,9 @@ from pelicanconf import *
 SITEURL = 'https://nacnudus.github.io/opre'
 RELATIVE_URLS = False
 
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
