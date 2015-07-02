9ec9bb66c065705cbf7da2bceed674b020230148
nacnudus <nacnudus@gmail.com>
2015-07-02 22:06:18 +1200

    Personalise the blogroll

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,10 +20,8 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('R-bloggers', 'http://www.r-bloggers.com/'),
+         ('StatsBlogs', 'www.statsblogs.com'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
