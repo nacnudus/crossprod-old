7bf7ec9492d29ae62bb1880695dd12c25c40a284
nacnudus <nacnudus@gmail.com>
2015-07-10 21:53:54 +1200

    Define sitemap

--- pelicanconf.py
+++ pelicanconf.py
@@ -71,3 +71,16 @@ SOCIAL = (('Github', 'https://github.com/nacnudus'),
           ('Twitter', 'https://twitter.com/nacnudus'),
           ('LinkedIn', 'https://nz.linkedin.com/in/duncangarmonsway'),)
 
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
